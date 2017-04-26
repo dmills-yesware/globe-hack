af6dcb0cfe218af2a578bb83b27c9eff846b7127
David Mills <dmills@yesware.com>
2017-04-26 17:53:42 -0400

    Hello World! (more literally than usual)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
+require("./styles/main.scss");
 import * as d3 from 'd3';
+import * as topojson from 'topojson';
 
-const square = d3.selectAll("rect");
-square.style("fill", "blue");
+let canvas = d3.select("#world-map");
+let width = canvas.property("width");
+let height = canvas.property("height");
+let context = canvas.node().getContext("2d");
+
+let projection = d3.geoOrthographic()
+    .translate([width / 2, height / 2])
+    .precision(0.1);
+
+let path = d3.geoPath()
+    .projection(projection)
+    .context(context);
+
+d3.json("data/world-50m.json", function(error, world) {
+    if (error) throw error;
+
+    var sphere = { type: "Sphere" };
+    var land = topojson.feature(world, world.objects.land);
+
+    let render = function() {
+        context.clearRect(0, 0, width, height);
+
+        // White-out the area where we're drawing the earth
+        context.beginPath();
+        path(sphere);
+        context.fillStyle = "#fff";
+        context.fill();
+
+        // Draw the land
+        context.beginPath();
+        path(land);
+        context.fillStyle = "#000";
+        context.fill();
+
+        // Draw the circle around the outside
+        context.beginPath();
+        path(sphere);
+        context.stroke();
+    };
+
+    render();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
-const path = require('path');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-});
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        path: path.resolve('dist'),
-        filename: 'index_bundle.js'
+        path: path.resolve("dist"),
+        filename: "index_bundle.js"
     },
     module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
-        ]
+        loaders: [{
+            test: /\.js$/,
+            loader: "babel-loader",
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            loaders: ['style-loader', 'css-loader', 'sass-loader']
+        }]
     },
-    plugins: [HtmlWebpackPluginConfig]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+            inject: "body"
+        })
+    ]
 };
