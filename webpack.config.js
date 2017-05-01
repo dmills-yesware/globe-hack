38ab3847ade5a24017fa70243d7ca2ed65b960b6
David Mills <dmills@yesware.com>
2017-05-01 13:54:10 -0400

    Use express for serving the app on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     devtool: "source-map",
@@ -37,6 +38,9 @@ module.exports = {
             filename: "hello-world.html",
             chunks: ["hello-world"],
             inject: "body"
-        })
+        }),
+        new CopyWebpackPlugin([
+            { from: "data", to: "data" }
+        ])
     ]
 };
